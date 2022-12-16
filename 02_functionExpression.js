494a278874b3269fdcb47fff811898f3c0dbc6a1
aishwryapawar <aishwayarpawar1997@gmail.com>
2022-12-16 09:53:27 +0530

    02_functionExpression.js updated
    02_functionExpression.js updated

---
+++ 02_functionExpression.js
@@ -0,0 +1,19 @@
+
+
+var name="Aishwarya";
+console.log(name);
+var showResult =function show()
+{
+   
+};
+console.log(showResult);
+
+
+var substraction= function(num1,num2)
+{
+    var result=num1-num2;
+    console.log("substraction...",result);
+    return result;
+}
+ var result=substraction(50,40);
+ console.log(sresult);
