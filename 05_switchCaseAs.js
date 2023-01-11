2e0c59168b7bd7006c499f5e0fe75e8b14dc07b2
aishwryapawar <aishwayarpawar1997@gmail.com>
2023-01-11 18:46:38 +0530

    05_switchCaseAs.js modified
    05_switchCaseAs.js modified

---
+++ 05_switchCaseAs.js
@@ -0,0 +1,75 @@
+console.log(`============= Switch Case Assignment =================`);
+console.log(`============== Month List============== `);
+
+var month = function (numMonth) {
+ 
+  switch (numMonth) {
+    case 1:
+      console.log(`January`);
+      break;
+
+    case 2:
+      console.log(`February`);
+      break;
+
+    case 3:
+      console.log(`March`);
+      break;
+
+    case 4:
+      console.log(`April`);
+      break;
+
+    case 5:
+      console.log(`May`);
+      break;
+
+    case 6:
+      console.log(`June`);
+      break;
+
+    case 7:
+      console.log(`July`);
+      break;
+
+    case 8:
+      console.log(`August`);
+      break;
+
+    case 9:
+      console.log(`September`);
+      break;
+
+    case 10:
+      console.log(`Octomber`);
+      break;
+
+    case 11:
+      console.log(`November`);
+      break;
+
+    case 12:
+      console.log(`December`);
+      break;
+
+    
+    default:
+        console.log(`Invalid Values ${numMonth} Entered please Try Again`);
+      
+      break;
+  }
+};
+month(1);
+month(2);
+month(3);
+month(4);
+month(5);
+month(6);
+month(7);
+month(8);
+month(9);
+month(10);
+month(11);
+month(12);
+month(13);
+month(14);
